f4a286b75164a8dec49524dfbb02a934dfecf7a1
eddiezhaor <eddiezhaor@gmail.com>
2018-03-07 16:21:18 -0600

    i AM HUNGRY

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "119110",
-    port: 3306,
-    database: "burgers_db"
-})
-
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "119110",
+        port: 3306,
+        database: "burgers_db"
+    })
+}
+connection.connect();
 module.exports = connection;
